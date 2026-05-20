2aed838df0d86ee822b90996f1f98d5b39ca6d8d
Ramtin <ramtin.react@gmail.com>
2026-05-20 14:34:54 +0330

    reservation has been removed instead we have appointments

--- app/reservations/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReservationsPageClient from "@/components/reservations/ReservationsPageClient";
-
-export default function ReservationsPage() {
-  return React.createElement(ReservationsPageClient);
-}
